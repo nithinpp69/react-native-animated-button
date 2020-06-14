ee37991f8b45a02702a69d846668e88e5a67f1b1
nithinpp69 <nithinpp69@gmail.com>
2020-06-14 15:35:38 +0530

    functional component

--- src/index.js
+++ src/index.js
@@ -1,51 +1,43 @@
-import React, {
-  Component
-} from 'react';
-import {
-  View,
-  Animated,
-  Easing,
-  TouchableOpacity
-} from 'react-native';
+import React from 'react';
+import { Animated, Easing, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 
-class AnimatedButton extends Component {
-  constructor(props) {
-    super(props);
-    this.animate = new Animated.Value(0);
-    this.animateButton = new Animated.Value(0);
-    this.actionOpacity = this.animate.interpolate({
-      inputRange: [0, 1],
-      outputRange: [0, 1],
-      extrapolate: 'clamp',
-    });
-    this.positionY = this.animate.interpolate({
-      inputRange: [0, 1],
-      outputRange: [0, this.props.direction === 'down' ? 100 : -100],
-      extrapolate: 'clamp'
-    });
-    this.scale = this.animate.interpolate({
-      inputRange: [0, 1],
-      outputRange: [0.65, 0.85],
-      extrapolate: 'clamp'
-    });
-    this.buttonScale = this.animateButton.interpolate({
-      inputRange: [0, 1],
-      outputRange: [1, 0.8],
-      extrapolate: 'clamp'
-    });
-  }
+const AnimatedButton = (props) => {
 
-  animateValue() {
+  const animate = new Animated.Value(0);
+  const animateButton = new Animated.Value(0);
+  const DISTANCE = 120;
+  const actionOpacity = animate.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1],
+    extrapolate: 'clamp'
+  });
+  const positionY = animate.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, props.direction === 'down' ? DISTANCE : -DISTANCE],
+    extrapolate: 'clamp'
+  });
+  const scale = animate.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0.65, 0.85],
+    extrapolate: 'clamp'
+  });
+  const buttonScale = animateButton.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0.8],
+    extrapolate: 'clamp'
+  });
+
+  const animateValue = () => {
     Animated.parallel([
       Animated.sequence([
-        Animated.timing(this.animate, {
+        Animated.timing(animate, {
           toValue: 1,
           duration: 600,
           easing: Easing.linear,
           useNativeDriver: true
         }),
-        Animated.timing(this.animate, {
+        Animated.timing(animate, {
           toValue: 0,
           duration: 10,
           easing: Easing.linear,
@@ -53,71 +45,45 @@ class AnimatedButton extends Component {
         })
       ]),
       Animated.sequence([
-        Animated.timing(this.animateButton, {
+        Animated.timing(animateButton, {
           toValue: 1,
           duration: 200,
           useNativeDriver: true
         }),
-        Animated.timing(this.animateButton, {
+        Animated.timing(animateButton, {
           toValue: 0,
           duration: 0,
           useNativeDriver: true
         })
       ])
     ]).start();
-  }
+  };
 
-  render() {
-    return (
-      <View>
-        <TouchableOpacity activeOpacity={0.8}
-          onPress={
-            () => {
-              this.animateValue(), this.props.onPress();
-            }
-          }
-        >
-          <Animated.View style={[this.props.containerStyle, {transform: [{scale: this.buttonScale}]}]}>
-             {
-              ...this.props.children
-            }
-          </Animated.View>
-        </TouchableOpacity>
-        <Animated.View style={
-          [{
-            opacity: this.actionOpacity,
-            position: 'absolute',
-            top: 0,
-            alignSelf: 'center',
-            transform: [{
-              translateY: this.positionY
-            }, {
-              scale: this.scale
-            }]
-          }]
-        }> {
-            this.props.infoContainer || this.props.children
-          }
-        </Animated.View>
-      </View >
-    );
-  }
-}
-
-AnimatedButton.propTypes = {
-  onPress: PropTypes.func.isRequired,
-  containerStyle: PropTypes.object,
-  infoContainer: PropTypes.element,
-  direction: PropTypes.oneOfType(['up', 'down']),
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired
+  return (
+    <>
+      <Animated.View pointerEvents={'none'} style={[props.containerStyle, {position: 'absolute', alignItems: 'center', justifyContent:'center', opacity: actionOpacity,transform: [{translateY: positionY}, {scale: scale}]}]}>{props.infoContainer || props.children}</Animated.View>
+      <TouchableOpacity activeOpacity={0.8} onPress={() => {animateValue(), props.onPress();}} style={props.containerStyle}>
+        <Animated.View style={{transform: [{scale: buttonScale}]}}>{props.children}</Animated.View>
+      </TouchableOpacity>
+    </>
+  );
 };
 
-Animated.defaultProps = {
+AnimatedButton.propTypes = {
+  onPress: PropTypes.func,
+  infoContainer: PropTypes.node,
+  direction: PropTypes.oneOfType(['up', 'down']),
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+  containerStyle: PropTypes.object
+};
+
+AnimatedButton.defaultProps = {
   containerStyle: {},
-  direction: 'up'
+  direction: 'up',
+  onPress: ()=>{}
 };
 
 export default AnimatedButton;
