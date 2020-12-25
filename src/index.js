7e1f07df4b7f06444ad5e00f15251d95d94b925d
nithinpp69 <nithinpp69@gmail.com>
2020-12-25 16:39:52 +0530

    move to reanimated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import PropTypes from 'prop-types';
 import Animated, { interpolate } from "react-native-reanimated";
